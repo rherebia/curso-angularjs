b625531b43abeea3c4f67a3023f5d4e0508ef0fe
Renato Herebia <rherebia@gmail.com>
2017-01-24 08:13:17 -0300

    Assistindo aula 5

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -16,4 +16,18 @@ angular.module('minhasDiretivas', [])
         ddo.templateUrl = 'js/directives/meu-painel.html';
         
         return ddo;
+    })
+    .directive('minhaFoto', function() {
+        var ddo = {};
+    
+        ddo.restrict = "AE";
+    
+        ddo.scope = {
+            url : '@',
+            titulo : '@'
+        }
+        
+        ddo.templateUrl = 'js/directives/minha-foto.html'
+    
+        return ddo;
     });
